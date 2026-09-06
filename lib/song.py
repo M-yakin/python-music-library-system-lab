45beca03ea352c12c2798f2c743869a231a4db33
Yakin <yakinm@DESKTOP-JM2PHG>
2026-09-06 16:19:03 +0300

    Fix: use artist_count (singular) per CodeGrade tests

--- lib/song.py
+++ lib/song.py
@@ -4,7 +4,7 @@ class Song:
     genres = set()
     artists = set()
     genre_count = {}
-    artists_count = {}
+    artist_count = {}
     
     def __init__(self, name, artist, genre):
         # Instance attributes - unique to each song
@@ -17,7 +17,7 @@ class Song:
         Song.add_to_genres(genre)
         Song.add_to_artists(artist)
         Song.add_to_genre_count(genre)
-        Song.add_to_artists_count(artist)
+        Song.add_to_artist_count(artist)
     
     @classmethod
     def add_song_to_count(cls):
@@ -43,9 +43,9 @@ class Song:
             cls.genre_count[genre] = 1
     
     @classmethod
-    def add_to_artists_count(cls, artist):
-        """Update artists_count dictionary - increment if exists, create if new"""
-        if artist in cls.artists_count:
-            cls.artists_count[artist] += 1
+    def add_to_artist_count(cls, artist):
+        """Update artist_count dictionary - increment if exists, create if new"""
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
         else:
-            cls.artists_count[artist] = 1
+            cls.artist_count[artist] = 1
